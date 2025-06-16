a1f50888389d9a1a28868e82f0c4ea0fecef315c
FurioStyle <benjamin.bglh@gmail.com>
2025-06-16 14:46:13 -0400

    feat: creacion de torneos y confirmacion simple

--- java/Logica/CrearTorneo.java
+++ java/Logica/CrearTorneo.java
@@ -1,4 +1,29 @@
 package Logica;
+import Logica.TiposTorneo.*;
 
 public class CrearTorneo {
+    private int tipoTorneo;
+    private int  numParticipantes;
+    private Torneo torneo;
+
+    public CrearTorneo(int t, int p) {
+        this.tipoTorneo = t;
+        this.numParticipantes = p;
+
+        switch (tipoTorneo){
+            case (1):
+                torneo = new Bracket(numParticipantes);
+                System.out.println("Nuevo torneo creado, Bracket");
+                break;
+            case (2):
+                torneo = new Liguilla(numParticipantes);
+                System.out.println("Nuevo torneo creado, Liguilla");
+                break;
+            case (3):
+                torneo = new Suizo(numParticipantes);
+                System.out.println("Nuevo torneo creado, Suizo");
+                break;
+        }
+    }
+
 }
