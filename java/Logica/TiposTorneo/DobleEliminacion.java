690bf2b800dfd79d6d4a4c810920fa1dd9a7c4ad
FurioStyle <benjamin.bglh@gmail.com>
2025-06-22 20:04:47 -0400

    feat: nuevo tipo de torneo

---
+++ java/Logica/TiposTorneo/DobleEliminacion.java
@@ -0,0 +1,8 @@
+package Logica.TiposTorneo;
+
+/**
+ * Genera un torneo con la mecanica doble eliminacion, donde existen dos brackets, el upper bracket, y
+ * el lower bracket, para que finalmente los ganadores de ambos brackets se enfrenten en la final.
+ */
+public class DobleEliminacion {
+}
