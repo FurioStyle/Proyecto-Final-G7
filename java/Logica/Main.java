a07d4816e22cf40450355adc2f9654cee5f8ab1b
FurioStyle <benjamin.bglh@gmail.com>
2025-06-22 20:13:49 -0400

    feat: creacion de un torneo de prueba

--- java/Logica/Main.java
+++ java/Logica/Main.java
@@ -1,8 +1,20 @@
 package Logica;
 
+import Logica.TiposTorneo.Torneo;
+
 public class Main {
     public static void main(String[] args) {
 
-        CrearTorneo crearTorneo = new CrearTorneo(2,8);
+        CrearTorneo crearTorneo = new CrearTorneo(1,6);
+        Torneo t = crearTorneo.getTorneo();
+
+        t.addPersona("Pepe",1);
+        t.addPersona("Pepa",2);
+        t.addPersona("Pepon",3);
+        t.addPersona("Pepito",4);
+        t.addPersona("Bob", 5);
+        t.addPersona("Alice",6);
+
+        System.out.println(t.generarTorneo());
     }
 }
