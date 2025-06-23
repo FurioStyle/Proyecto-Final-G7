73ef694b9009987c1682218fa7ea56b190a6d7e0
FurioStyle <benjamin.bglh@gmail.com>
2025-06-22 20:06:15 -0400

    feat: avance en el esqueleto de Liguilla para contener los metodos abstractos

--- java/Logica/TiposTorneo/Liguilla.java
+++ java/Logica/TiposTorneo/Liguilla.java
@@ -1,8 +1,35 @@
 package Logica.TiposTorneo;
 
+import Logica.Participantes.Equipo;
+import Logica.Participantes.Participante;
+import Logica.Participantes.Persona;
+
+import java.util.ArrayList;
+
+/**
+ * Genera un torneo ordenado por puntos, quien tenga mas puntos gana.
+ */
 public class Liguilla extends Torneo{
+    protected ArrayList<Participante> participantes;
 
     public Liguilla(int x){
         super(x);
+        this.participantes = new ArrayList<>();
+    }
+
+    @Override
+    public void addPersona(String nombre, int id){
+        Participante p = new Persona(nombre,id);
+        this.participantes.add(p);
+    }
+    @Override
+    public void addEquipo(String nombre, int id){
+        Equipo p = new Equipo(nombre,id);
+        this.participantes.add(p);
+    }
+
+    @Override
+    public String generarTorneo() {
+        return "";
     }
 }
