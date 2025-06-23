b9f2aeaab3de2e63d0b10a4de9373448a8eacda9
FurioStyle <benjamin.bglh@gmail.com>
2025-06-22 20:05:35 -0400

    feat: agregados nuevos metodos abstractos para agregar participantes a los torneos

--- java/Logica/TiposTorneo/Torneo.java
+++ java/Logica/TiposTorneo/Torneo.java
@@ -4,9 +4,14 @@ public abstract class Torneo {
     private int participantes;
     public Torneo(int x){
         this.participantes = x;
-    }
 
-    public int getParticipantes(){
+    }
+    public abstract void addPersona(String nombre, int id);
+
+    public abstract void addEquipo(String nombre, int id);
+
+    public int getNumParticipantes(){
         return participantes;
     }
+    public abstract String generarTorneo();
 }
