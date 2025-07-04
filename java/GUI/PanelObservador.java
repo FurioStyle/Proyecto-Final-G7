088a85f43a14865bd6d145f35eb269b8dcb04d31
FurioStyle <benjamin.bglh@gmail.com>
2025-07-04 18:58:54 -0400

    feat: nueva logica para usar junto a las listas creadas en PanelAdmin, y boton para refrescar las listas

--- java/GUI/PanelObservador.java
+++ java/GUI/PanelObservador.java
@@ -1,28 +1,89 @@
 package GUI;
 
+import Logica.TiposTorneo.Torneo;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class PanelObservador extends JPanel {
-    private JButton btnActualizarTorneo;
-    private JButton btnVolver;
+    private JButton bVer;
+    private JButton bActualizar;
+    private JButton bVolver;
 
-    public PanelObservador(VentanaPrincipal ventana) {
+    private DefaultListModel<Torneo> modeloEnCurso;
+    private DefaultListModel<Torneo> modeloFinalizados;
+
+    private JList<Torneo> listaEnCurso;
+    private JList<Torneo> listaFinalizados;
+
+    private PanelAdmin panelAdmin;
+
+    public PanelObservador(VentanaPrincipal ventana, PanelAdmin panelAdmin) {
+        this.panelAdmin = panelAdmin;
+
         setLayout(new BorderLayout());
 
-        JPanel Botones = new JPanel();
-        Botones.setLayout(new BoxLayout(Botones, BoxLayout.Y_AXIS));
+        modeloEnCurso = new DefaultListModel<>();
+        modeloFinalizados = new DefaultListModel<>();
 
-        btnActualizarTorneo = new JButton("Ver Torneo");
-        btnVolver = new JButton("Volver");
+        listaEnCurso = new JList<>(modeloEnCurso);
+        listaFinalizados = new JList<>(modeloFinalizados);
 
-        Botones.add(Box.createVerticalStrut(10));
-        Botones.add(btnActualizarTorneo);
-        Botones.add(Box.createVerticalStrut(10));
-        Botones.add(btnVolver);
-        btnVolver.addActionListener(e -> ventana.mostrarPanel("inicio"));
-        Botones.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        JPanel panelBotones = new JPanel();
+        panelBotones.setLayout(new BoxLayout(panelBotones, BoxLayout.Y_AXIS));
 
-        add(Botones, BorderLayout.WEST);
+        bVer = new JButton("Ver Torneo");
+        bActualizar = new JButton("Actualizar");
+        bVolver = new JButton("Volver");
+
+        bVer.addActionListener(e -> {
+            Torneo seleccionado = listaEnCurso.getSelectedValue();
+            if (seleccionado != null) {
+                JOptionPane.showMessageDialog(this, "Seleccionaste: " + seleccionado.getNombre());
+            }
+        });
+
+        bActualizar.addActionListener(e -> {
+            actualizarListas();
+        });
+
+        bVolver.addActionListener(e -> {
+            ventana.mostrarPanel("inicio");
+        });
+
+        panelBotones.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        panelBotones.add(bVer);
+        panelBotones.add(Box.createVerticalStrut(10));
+        panelBotones.add(bActualizar);
+        panelBotones.add(Box.createVerticalStrut(10));
+        panelBotones.add(bVolver);
+
+        JPanel panelLista = new JPanel(new GridLayout(2, 1, 10, 10));
+
+        JPanel panelLista1 = new JPanel(new BorderLayout());
+        panelLista1.setBorder(BorderFactory.createTitledBorder("Torneos En Curso"));
+        panelLista1.add(new JScrollPane(listaEnCurso), BorderLayout.CENTER);
+
+        JPanel panelLista2 = new JPanel(new BorderLayout());
+        panelLista2.setBorder(BorderFactory.createTitledBorder("Torneos Finalizados"));
+        panelLista2.add(new JScrollPane(listaFinalizados), BorderLayout.CENTER);
+
+        panelLista.add(panelLista1);
+        panelLista.add(panelLista2);
+
+        add(panelBotones, BorderLayout.WEST);
+        add(panelLista, BorderLayout.CENTER);
+    }
+
+    public void actualizarListas() {
+        modeloEnCurso.clear();
+        for (Torneo t : panelAdmin.getEnCurso()) {
+            modeloEnCurso.addElement(t);
+        }
+
+        modeloFinalizados.clear();
+        for (Torneo t : panelAdmin.getFinalizados()) {
+            modeloFinalizados.addElement(t);
+        }
     }
 }
