4dedbe7b7152255202845820968c47902d43ed5b
FurioStyle <benjamin.bglh@gmail.com>
2025-07-13 17:58:18 -0400

    feat: mejora de logica y adicion de metodos abstractos de la super clase

--- java/Logica/TiposTorneo/DobleEliminacion.java
+++ java/Logica/TiposTorneo/DobleEliminacion.java
@@ -11,21 +11,49 @@ import java.util.ArrayList;
  * el lower bracket, para que finalmente los ganadores de ambos brackets se enfrenten en la final.
  */
 public class DobleEliminacion extends Torneo{
-    private ArrayList<Participante> participantes;
+    public ArrayList<Participante> participantes;
+    public ArrayList<Participante> pActivos;
+    private ArrayList<Participante> upperBracket;
+    private ArrayList<Participante> lowerBracket;
+    private int numParticipantes;
 
     public DobleEliminacion(int x, String n){
         super(n);
+        this.numParticipantes = x;
         this.participantes = new ArrayList<>();
+        this.pActivos = new ArrayList<>();
     }
 
     @Override
-    public void addPersona(String nombre, int id){
+    public void addPersona(String nombre, String id){
         Participante p = new Persona(nombre,id);
         this.participantes.add(p);
+        pActivos.add(p);
     }
     @Override
-    public void addEquipo(String nombre, int id){
+    public void addEquipo(String nombre, String id){
         Equipo p = new Equipo(nombre,id);
-        this.participantes.add(p);
+        participantes.add(p);
+        pActivos.add(p);
+    }
+
+    @Override
+    public int getNumParticipantes(){
+        return numParticipantes;
+    }
+
+    @Override
+    public ArrayList<Participante> getParticipantes(){
+        return participantes;
+    }
+
+    @Override
+    public ArrayList<Participante> getPActivos() {
+        return pActivos;
+    }
+
+    @Override
+    public void pasarRondas(ArrayList<Participante> ganadores){
+        pActivos = new ArrayList<>(ganadores);
     }
 }
